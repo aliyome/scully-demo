d50fae9786531d3247696273245457411ac5866f
aliyome <aliyome@gmail.com>
2019-12-17 10:55:36 +0900

    feat: Add static routes to user 1, 2, 3, and so on.

--- scully.config.js
+++ scully.config.js
@@ -1,4 +1,12 @@
 exports.config = {
   projectRoot: "./src/app",
-  routes: {}
+  routes: {
+    "/user/:userId": {
+      type: "json",
+      userId: {
+        url: "https://jsonplaceholder.typicode.com/users",
+        property: "id"
+      }
+    }
+  }
 };
