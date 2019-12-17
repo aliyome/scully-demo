97abd9d428318a76411c10f0b9d1203151aad7a6
aliyome <aliyome@gmail.com>
2019-12-17 10:34:17 +0900

    setup: ng add @scully/init (@0.0.7)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { IdleMonitorService } from '@scullyio/ng-lib';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  constructor(private idle: IdleMonitorService) {}
+
   title = 'scully-demo';
 }
