63fc0426f65ced669ad90cacfdd8d9a26966e594
aliyome <aliyome@gmail.com>
2019-12-17 10:14:24 +0900

    feat: Add UsersComponent, and modify route.

--- src/app/user/user-routing.module.ts
+++ src/app/user/user-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { UsersComponent } from './users.component';
 import { UserComponent } from './user.component';
 
 const routes: Routes = [
   {
     path: '',
+    component: UsersComponent,
+  },
+  {
+    path: ':id',
     component: UserComponent,
   },
 ];
--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { UserRoutingModule } from './user-routing.module';
 import { UserComponent } from './user.component';
+import { UsersComponent } from './users.component';
 
 @NgModule({
-  declarations: [UserComponent],
+  declarations: [UserComponent, UsersComponent],
   imports: [CommonModule, UserRoutingModule],
 })
 export class UserModule {}
