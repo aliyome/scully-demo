093a9262f46a5beaf303f897b22182df1f7deaca
aliyome <aliyome@gmail.com>
2019-12-17 10:05:44 +0900

    feat: Add user, home and about to the router

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -26,8 +26,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain(
-      'scully-demo app is running!',
-    );
+    expect(compiled.querySelector('h1').textContent).toContain('Scully demo');
   });
 });
