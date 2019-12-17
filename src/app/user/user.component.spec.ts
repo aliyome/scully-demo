53a8a5138bab53740a7d3ba29cfff0845a50a7e2
aliyome <aliyome@gmail.com>
2019-12-17 10:27:00 +0900

    fix: Fix failed test

--- src/app/user/user.component.spec.ts
+++ src/app/user/user.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { UserComponent } from './user.component';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('UserComponent', () => {
   let component: UserComponent;
@@ -9,6 +10,7 @@ describe('UserComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [UserComponent],
+      imports: [RouterTestingModule],
     }).compileComponents();
   }));
 
